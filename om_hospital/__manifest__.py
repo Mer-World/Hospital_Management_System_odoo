a626ccf47db390a2d615e3a017d66a03e4992bb3
Meronawit Dejene <150444100+Mer-World@users.noreply.github.com>
2025-08-18 11:03:23 +0300

    Create __manifest__.py

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',#since its first,optional
+    'summary': 'Hospital Management System',
+    'description': """
+    Hospital Management System
+    """,
+    'author': 'Meronawit Dejene',
+    'category': 'Hospital',
+    'depends': [], 
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'assets':{},
+}
