33f8b6ab82a3b2d07e3c314ab257412bf43462eb
Meronawit Dejene <150444100+Mer-World@users.noreply.github.com>
2025-08-25 14:48:18 +0300

    Update patient.py

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -2,10 +2,13 @@ from odoo import models, fields, api
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'  # Model name
+    _inherit = ['mail.thread', 'mail.activity.mixin']  
     _description = 'Hospital Patient'
 
-    name = fields.Char(string='Name', required=True)
-    age = fields.Integer(string='Age')
+    name = fields.Char(string='Name', required=True,tracking=True)
+    age = fields.Integer(string='Age',tracking=True)
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),], string='Gender', required=True)
+    active = fields.Boolean(string="Active",default=True)
+    
