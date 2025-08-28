6366c2d7a38f21436aa1838065d735fd143c3562
Meronawit Dejene <150444100+Mer-World@users.noreply.github.com>
2025-08-28 08:34:59 +0300

    Update appointment.py

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -1,1 +1,12 @@
 
+from odoo import models, fields, api
+
+class HospitalAppointment(models.Model):
+    _name = 'hospital.appointment'  # Model name
+    _inherit = ['mail.thread', 'mail.activity.mixin']  
+    _description = 'Hospital Appointment'
+
+
+    patient_id =fields.Many2one("hospital.patient",string="Patient")
+    appointment_time = fields.Datetime(string="Appointment Time")
+    booking_date = fields.Date(string="Booking Date")
